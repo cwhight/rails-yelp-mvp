fa28cbc6a30b24442833da94caef85dfe5dca94f
Charlie Whight <cdwhight@gmail.com>
2019-11-13 12:26:35 +0000

    basic add review with no styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  # get 'reviews/index'
+  # get 'reviews'
+  # get 'reviews/show'
+  # get 'reviews/new'
+  # get 'reviews/edit'
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
+  # get 'restaurants/edit'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create, :index]
+  end
+
+  resources :reviews, except: [:new, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
